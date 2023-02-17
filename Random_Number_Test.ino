5ef35f949800e7b4258c408e9de95c0cc169ec2c
ScottBillingsley <61607063+ScottBillingsley@users.noreply.github.com>
2023-02-17 15:52:37 -0600

    Add files via upload

---
+++ Random_Number_Test.ino
@@ -0,0 +1,144 @@
+/*
+                        Random Numner Test
+                     Vernon Billingsley c2023
+                        Raspberry Pi Pico
+                       Arduino IDE 1.8.13                     
+
+                 Seup the random number generator using the 
+                 ring oscillator to generate a random seed..     
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission
+    notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+               Raspberry Pi Pico
+
+               -----------------
+              |      |USB|      |
+ TXD 0 GPIO0 -|1              40|-  VBUS
+ RXD 0 GPIO1 -|2              39|-  Vsys
+       GND   -|3              38|-  GND
+       GPIO2 -|4              37|-  EN
+       GPIO3 -|5              36|-  3.3v Out
+       GPIO4 -|6              35|-  ADC Ref
+       GPIO5 -|7              34|-  GPIO28  ADC2
+       GND   -|8              33|-  GND AGND
+       GPIO6 -|9              32|-  GPIO27  ACD1
+       GPIO7 -|10             31|-  GPIO26
+       GPIO8 -|11             30|-  Run/Reset
+       GPIO9 -|12             29|-  GPIO22
+       GND   -|13             28|-  GND
+      GPIO10 -|14             27|-  GPIO21
+      GPIO11 -|15             26|-  GPIO20
+      GPIO12 -|16             25|-  GPIO19
+      GPIO13 -|17             24|-  GPIO18
+       GND   -|18             23|-  GND
+      GPIO14 -|19             22|-  GPIO17
+      GPIO15 -|20             21|-  GPIO16
+               -----------------
+
+  Board json..
+  https://github.com/earlephilhower/arduino-pico
+
+  Board Settings:
+  Raspberry Pi Pico
+  2MB (on FS)
+  133 mHz
+  Small (-Os) (standard)
+  RTTI: disabled
+  Stack Protector: disabled
+  C++ Exceptions: disabled
+  Debug Port: disabled
+  Debug Level: none
+  USB Stack: "Pico SDK"
+  IP Stack: "IPv4 Only"
+  Upload Method: "Default (UF2)"
+
+  Ring Oscillator random number generator code:
+  https://deemocean.com/2022/10/02/rp2040-randomness-and-ring-oscillator/
+
+*/
+/************************ Includes ********************************/
+
+
+/************************* Defines ********************************/
+#if !defined(LED_BUILTIN)
+#define LED_BUILTIN     25
+#endif
+
+#define DEBUG 1
+
+#if DEBUG > 0
+#define dprint(expression) Serial.print("# "); Serial.print( #expression ); Serial.print( ": " ); Serial.println( expression )
+#define dshow(expression) Serial.print( expression )
+#else
+#define dprint(expression)
+#define dshow(expression)
+#endif
+
+/* Address of the Ring Oscillator bit */
+#define ROSC_RANDOMBIT_OFFSET _u(0x0000001c)
+#define ROSC_BASE _u(0x40060000)
+
+/************************** Variables *****************************/
+
+/**************************  Functions ****************************/
+/* Bits between 0 and 32 */
+uint32_t ring_osc_rand(uint8_t bits) {
+  static volatile uint32_t *randbit_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+  uint32_t random = 0;
+  for (int i = 0; i < bits; i++)
+  {
+    unsigned int random_bit = 0x1 & (*randbit_reg);
+    random = random << 1 | random_bit;
+  }
+  return random;
+}
+
+
+/******************************************************************/
+/*************************** Setup ********************************/
+/******************************************************************/
+void setup() {
+  if (DEBUG > 0) {
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.print(F(BOARD_NAME)); Serial.print(" @ "); Serial.print(F_CPU); Serial.println(" mHz");
+  }
+  /* Get a rnadom seed from the Ring Oscillator */
+  uint32_t random_seed = ring_osc_rand(32);
+  /* Intialize the RNG */
+  srand(random_seed);
+  
+
+}/**************************  End Setup **************************/
+
+/******************************************************************/
+/**************************** Loop ********************************/
+/******************************************************************/
+void loop() {
+  /* Print 10 random numbers from 0 to 100 */
+  for(uint8_t r = 0; r < 10; r ++){
+    Serial.println(rand() % 100);
+    /* Delay a little between prints */
+    delay(25);
+  }
+
+  /* Wait forever */
+  while(1){}
+
+}/*************************** End Loop *****************************/
